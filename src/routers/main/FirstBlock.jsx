b903ae07db7a40b1a7284b5307267aecc2c9fd48
Trooman <troomant.the@gmail.com>
2021-12-28 22:13:34 +0200

    animation on first block

--- src/routers/main/FirstBlock.jsx
+++ src/routers/main/FirstBlock.jsx
@@ -1,12 +1,13 @@
 import Site from "./img/Site";
 import "./styles/main.css"
 import "./styles/firstBlock.css"
+import 'animate.css';
 
 export default function FirstBlock() {
     return (
         <section className={"firstBlock"}>
             <div className={"textBlock"}>
-                <h1>
+                <h1 className="animate__animated animate__fadeInBottomLeft">
                     РАЗРАБОТКА САЙТОВ
                     <br/>
                     С УНИКАЛЬНЫМ
@@ -14,12 +15,12 @@ export default function FirstBlock() {
                     <br/>
                     <span className={"emtyInsideTitle"}>ВЕБ-ДИЗАЙНОМ</span>
                 </h1>
-                <div className={"links"}>
+                <div className={"links animate__animated animate__bounceInUp"}>
                     <a href={"#"} className={"blueBorrder"}>Посмотреть работы</a>
                     <a href={"#"} className={"blueInside"}>Узнать стоимость</a>
                 </div>
             </div>
-            <div className={"SiteSvg"}>
+            <div className={"SiteSvg animate__animated animate__fadeInBottomRight"}>
             <Site/>
             </div>
         </section>
