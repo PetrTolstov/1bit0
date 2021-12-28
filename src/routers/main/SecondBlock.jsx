0e1aaebf703173221da117838339d9b61a0ea140
PetrTolstov <petr.tolstov@ivkhk.ee>
2021-12-28 23:32:22 +0300

    second block

--- src/routers/main/SecondBlock.jsx
+++ src/routers/main/SecondBlock.jsx
@@ -1,11 +1,12 @@
 import "./styles/main.css"
-import "./styles/firstBlock.css"
+import "./styles/secondBlock.css"
+import Lines from "./img/Lines";
 
 export default function SecondBlock() {
     return (
         <section className={"secondBlock"}>
             <div className={"animatedBlock"}>
-
+                <Lines/>
             </div>
             <div className={"textBlock"}>
 
