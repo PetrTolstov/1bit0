6cb20547cc3b23d979ff8aaaad6f161f6f90e52d
PetrTolstov <petr.tolstov@ivkhk.ee>
2021-12-28 23:09:34 +0300

    second block

--- src/routers/main/Main.jsx
+++ src/routers/main/Main.jsx
@@ -1,10 +1,11 @@
 import FirstBlock from "./FirstBlock";
-
+import SecondBlock from "./SecondBlock"
 
 export default function Main() {
     return (
                 <main>
                     <FirstBlock/>
+                    <SecondBlock/>
                 </main>
     )
 }
---
+++ src/routers/main/SecondBlock.jsx
@@ -0,0 +1,15 @@
+import "./styles/main.css"
+import "./styles/firstBlock.css"
+
+export default function SecondBlock() {
+    return (
+        <section className={"secondBlock"}>
+            <div className={"animatedBlock"}>
+
+            </div>
+            <div className={"textBlock"}>
+
+            </div>
+        </section>
+ )
+}
